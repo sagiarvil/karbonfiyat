d1f2a94e7c6fd5f7eba5ab0ed510c379a4c23ab8
sagi arvil <teb232@gmail.com>
2026-09-23 01:00:30 +0300

    seo: make sitemap freshness truthful

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -11,28 +11,25 @@ export async function GET() {
     "/workspace",
     "/carbon-monitor",
     "/fiyatlandirma",
-    "/metodoloji"
+    "/metodoloji",
+    "/hakkimizda",
+    "/iletisim",
+    "/gizlilik"
   ];
-
-  const now = new Date().toISOString().split("T")[0];
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${routes
-  .map(
-    (route) => `  <url>
+  .map((route) => `  <url>
     <loc>${baseUrl}${route}</loc>
-    <lastmod>${now}</lastmod>
-    <changefreq>${route === "" ? "daily" : "weekly"}</changefreq>
-    <priority>${route === "" ? "1.0" : "0.8"}</priority>
-  </url>`
-  )
+  </url>`)
   .join("\n")}
 </urlset>`;
 
   return new Response(xml, {
     headers: {
-      "Content-Type": "application/xml; charset=utf-8"
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=3600"
     }
   });
 }
